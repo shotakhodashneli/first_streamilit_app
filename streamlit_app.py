1c88e6f1bab000767ed02b6b737fb8317d4eae5a
shotakhodashneli <98159194+shotakhodashneli@users.noreply.github.com>
2022-07-13 12:30:48 +0400

    Update streamlit_app.py
    menu tabs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.title('omega 3 & Blueberry Oatmeal')
+streamlit.title('Kale, Spinach & ROcket Smoothie')
+streamlit.title('Hard-Boiled Free_Range Egg')
